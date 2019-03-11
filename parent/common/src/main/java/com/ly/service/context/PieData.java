a2fa30d8b9f0905e102e3dc109306ffb09340ff8
imoldxu <9794208@qq.com>
2019-03-11 23:20:50 +0800

    增加统计基础数据结构

---
+++ parent/common/src/main/java/com/ly/service/context/PieData.java
@@ -0,0 +1,25 @@
+package com.ly.service.context;
+
+public class PieData {
+
+	private String label;
+	
+	private Object data;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+}
