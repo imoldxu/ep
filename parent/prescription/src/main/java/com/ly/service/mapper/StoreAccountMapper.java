ca2dc7218609135195d6889204e99b7a4b2d2196
imoldxu <9794208@qq.com>
2019-03-11 23:34:40 +0800

    增加根据名称获取药房账户信息的sql

--- parent/prescription/src/main/java/com/ly/service/mapper/StoreAccountMapper.java
+++ parent/prescription/src/main/java/com/ly/service/mapper/StoreAccountMapper.java
@@ -1,8 +1,15 @@
 package com.ly.service.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.ly.BaseMapper;
 import com.ly.service.entity.StoreAccount;
 
 public interface StoreAccountMapper extends BaseMapper<StoreAccount> {
 
+	public List<StoreAccount> getStoreAccountByName(@Param(value="name") String name,
+			@Param(value="offset") Integer offset,
+			@Param(value="size") Integer size);
 }
